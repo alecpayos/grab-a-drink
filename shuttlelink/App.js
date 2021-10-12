8a78d6032bc7fa525f78399f004f81714111bcd1
alecpayos <alecpayos.2020@gmail.com>
2021-10-12 16:44:07 +0800

    layout stage

---
+++ shuttlelink/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styled from './App.Styles';
+
+const App = () => {
+    return (
+        <Styled.Container prop="wrapper">
+            <Styled.Container>
+                
+            </Styled.Container>
+        </Styled.Container>
+    );
+}
+
+export default App;
