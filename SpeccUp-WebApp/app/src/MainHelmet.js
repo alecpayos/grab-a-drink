cdfdf36162c94b01d446f638fa4db4162937b648
alecpayos <alecpayos.2020@gmail.com>
2021-11-21 20:54:48 +0800

    before backend integration

--- SpeccUp-WebApp/app/src/MainHelmet.js
+++ SpeccUp-WebApp/app/src/MainHelmet.js
@@ -8,6 +8,9 @@ const MainHelmet = () => {
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>SpeccUp</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&family=Roboto+Mono:wght@700&display=swap" rel="stylesheet" />
                 <script src="https://kit.fontawesome.com/6ab75e7019.js" crossorigin="anonymous"></script>
             </Helmet>
         </>
--- SpeccUp-WebApp/flask/flaskApp.py
+++ SpeccUp-WebApp/flask/flaskApp.py
@@ -1,31 +1,26 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='app/build', static_url_path='')
+app = Flask(__name__, static_url_path='')
 CORS(app)
 
-@app.route('/home')
-@cross_origin()
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route('/')
+def home():
+    return send_from_directory('templates', 'index.html')
 
 @app.route('/header')
-@cross_origin()
 def header():
     return send_from_directory('templates', 'header.html')
 
 @app.route('/content')
-@cross_origin()
-def home():
+def content():
     return send_from_directory('templates', 'content.html')
 
 @app.route('/footer')
-@cross_origin()
 def footer():
     return send_from_directory('templates', 'footer.html')
 
 @app.route('/styles')
-@cross_origin()
 def styles():
     return send_from_directory('static', 'styles.css')
 
