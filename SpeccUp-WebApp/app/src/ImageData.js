1a8b7e0ef3e10c2d35c47633d95064cc1f47e895
alecpayos <alecpayos.2020@gmail.com>
2021-12-13 17:24:57 +0800

    changed project name

--- SpeccUp-WebApp/app/src/ImageData.js
+++
@@ -1,7 +0,0 @@
-import FeatureImageOne from './assets/flex.png';
-import FeatureImageTwo from './assets/speed.png';
-import FeatureImageThree from './assets/squat.png'
-
-const ImageData = [FeatureImageOne, FeatureImageTwo, FeatureImageThree];
- 
-export default ImageData;
