1a8238cecb78a4499ab11f9cb2a14da7b825ded6
alecpayos <alec@getdentalray.com>
2024-02-21 17:26:39 +0800

    final cleanup for project

--- src/pages/Membership.js
+++ src/pages/Membership.js
@@ -12,9 +12,7 @@ export default function Membership () {
 
     return (
         <Grid container className={style.member}>
-            <Grid item lg={2}><h2>TESTING HIHIHI</h2></Grid>
-            <Grid item lg={2}><h2>TESTING HIHIHI</h2></Grid>
-            <Grid item lg={2}><h2>TESTING HIHIHI</h2></Grid>
+            <Grid item lg={2}><h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2></Grid>
         </Grid>
     );
 };
